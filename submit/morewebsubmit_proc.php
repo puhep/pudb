b03b8b4c1e73188f982d4766c2df41a03ec23ccc
Gregory W Neeser <gneeser@purdue.edu>
2015-11-18 16:42:19 -0500

    MoReWeb data submit requires filetype to be .tar

--- submit/morewebsubmit_proc.php
+++ submit/morewebsubmit_proc.php
@@ -7,11 +7,11 @@ $ext = $path_parts['extension'];
 
 if(isset($_POST['submit']) && $_FILES['tarball']['size'] > 0 && $ext == "tar"){
 	
-	#$dir = "/project/cmsfpix/.www/Submission_p/morewebInput/";
-	#$tmptar = $_FILES['tarball']['tmp_name'];
-	#$tar = $_FILES['tarball']['name'];
+	$dir = "/project/cmsfpix/.www/Submission_p/morewebInput/";
+	$tmptar = $_FILES['tarball']['tmp_name'];
+	$tar = $_FILES['tarball']['name'];
 
-	#move_uploaded_file($tmptar, $dir.$tar);
+	move_uploaded_file($tmptar, $dir.$tar);
 
 	#exec("tar -zxvf ".$dir.$tar);
 
