f8bb383d21f4d871fc609d39608c6fae894c7475
Gregory W Neeser <gneeser@purdue.edu>
2016-02-25 15:19:54 -0500

    Sort by module quality in rejected bin on tested modules list page

--- summary/test_list.php
+++ summary/test_list.php
@@ -549,7 +549,7 @@ $func4 = "SELECT a.name, a.id from module_p a, times_module_p b, ROC_p c WHERE a
 $func5 = "SELECT a.name, a.id from module_p a, times_module_p b, ROC_p c WHERE a.tested_status LIKE \"Final Judgement\" AND a.id=b.assoc_module AND a.id=c.assoc_module ".$sorter." GROUP BY a.name ORDER BY b.shipped DESC";
 $func6 = "SELECT a.name, a.id from module_p a, times_module_p b, ROC_p c WHERE a.tested_status LIKE \"Debugging\" AND a.id=b.assoc_module AND a.id=c.assoc_module ".$sorter." GROUP BY a.name ORDER BY b.shipped DESC";
 $func7 = "SELECT a.name, a.id from module_p a, times_module_p b, ROC_p c WHERE a.tested_status LIKE \"Ready for Mounting\" AND a.id=b.assoc_module AND a.id=c.assoc_module ".$sorter." GROUP BY a.name ORDER BY SUM(c.deadpix+c.badbumps_elec+c.unmaskable+c.unaddressable+IFNULL(c.vcal_thresh,0)) ASC";
-$func8 = "SELECT a.name, a.id from module_p a, times_module_p b, ROC_p c WHERE a.tested_status LIKE \"Rejected\" AND a.id=b.assoc_module AND a.id=c.assoc_module ".$sorter." GROUP BY a.name ORDER BY b.shipped DESC";
+$func8 = "SELECT a.name, a.id from module_p a, times_module_p b, ROC_p c WHERE a.tested_status LIKE \"Rejected\" AND a.id=b.assoc_module AND a.id=c.assoc_module ".$sorter." GROUP BY a.name ORDER BY SUM(IFNULL(c.deadpix,1000000)+IFNULL(c.badbumps_elec,1000000)+IFNULL(c.unmaskable,0)+IFNULL(c.unaddressable,0)+IFNULL(c.vcal_thresh,0)) ASC";
 $func9 = "SELECT a.name, a.id from module_p a, times_module_p b, ROC_p c WHERE a.tested_status IS NULL AND a.id=b.assoc_module AND a.id=c.assoc_module ".$sorter." GROUP BY a.name ORDER BY b.shipped DESC";
 
 
