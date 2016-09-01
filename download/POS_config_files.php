64c4f18836acce475450bd9db3e7ab8774357137
Gregory W Neeser <gneeser@purdue.edu>
2016-09-01 12:17:28 -0400

    Added POS Config page

---
+++ download/POS_config_files.php
@@ -0,0 +1,99 @@
+<html>
+<head>
+  <meta content="text/html; charset=ISO-8859-1"
+ http-equiv="content-type">
+  <title>POS Config Files</title>
+</head>
+<body>
+
+<?php
+include("../../../Submission_p_secure_pages/connect.php");
+include("../functions/curfunctions.php");
+ini_set('display_errors', 'On');
+error_reporting(E_ALL | E_STRICT);
+
+
+	$name = $_GET['name'];
+	$id = findid("module_p", $name);
+        $pos_name = find_name_pos($id);
+	$basedir = "../../Submission_p/POS_config_files/";
+
+	$dir = $basedir.$pos_name;
+	echo "<table>";
+	echo "<tr valign=top>";
+	echo "<th>";
+	echo "File";
+	echo "</th>";
+
+	echo "<th>";
+	echo "&nbsp";
+	echo "</th>";
+
+	echo "<th>";
+	echo "Size";
+	echo "</th>";
+	
+	echo "<th>";
+	echo "&nbsp";
+	echo "</th>";
+
+
+	echo "<th>";
+	echo "Timestamp";
+	echo "</th>";
+
+	echo "</tr>";
+
+
+	if(file_exists($dir) && ($handle = opendir($dir))){
+
+		while(false !== ($entry=readdir($handle))){
+
+			if($entry != "." && $entry != ".."){
+
+				echo "<tr valign=top>";
+			
+				echo "<td>";
+				echo "<a href=\"$dir/$entry\" target=\"_blank\">$entry</a>";;
+				echo "</td>";
+
+				echo "<td>";
+				echo "&nbsp";
+				echo "&nbsp";
+				echo "&nbsp";
+				echo "&nbsp";
+				echo "&nbsp";
+				echo "&nbsp";
+				echo "&nbsp";
+				echo "&nbsp";
+				echo "</td>";
+
+				echo "<td>";
+				echo human_filesize(filesize($dir."/".$entry));
+				echo "</td>";
+
+				echo "<td>";
+				echo "&nbsp";
+				echo "&nbsp";
+				echo "&nbsp";
+				echo "&nbsp";
+				echo "&nbsp";
+				echo "&nbsp";
+				echo "&nbsp";
+				echo "&nbsp";
+				echo "</td>";
+
+				echo "<td>";
+				echo date('Y-m-d H:i:s', filemtime($dir."/".$entry));
+				echo "</td>";
+
+				echo "</tr>";
+			}
+		}
+	}
+
+	echo"</table>";
+
+?>
+</body>
+</html>
